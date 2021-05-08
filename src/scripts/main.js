d43f9bf54caf65e577e6c55ce63b9754b11c29ad
Cristopher Alvear <c_alvear@live.cl>
2021-05-08 19:52:03 -0400

    feat: scroll position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 import DomHandler from "./dom-handler.js";
 import { randomNumber } from './utils.js';
 
+var topButton;
+
 window.onload = () => {
     // initializes grid DOM handler and
     // attaches it to global window element
@@ -9,4 +11,15 @@ window.onload = () => {
     // begins with random cards count
     domHandler.add(randomNumber(5, 20));
     domHandler.render();
+};
+
+window.onscroll = () => {
+    // gets top button DOM element
+    topButton = topButton ?? document.getElementById('top-button');
+
+    // validates scroll position
+    if (window.scrollX === 0 && window.scrollY)
+        topButton.classList.remove("hide");
+    else
+        topButton.classList.add("hide");
 };
